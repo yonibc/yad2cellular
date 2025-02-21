590b35521de0c236b566b87cdde36919edc70866
Yoni Katz <yehonatan.katz@biocatch.com>
2025-02-21 16:28:00 +0200

    1

--- app/src/main/java/com/example/yad2cellular/MainActivity.kt
+++ app/src/main/java/com/example/yad2cellular/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.yad2cellular
 
-import android.net.wifi.hotspot2.pps.HomeSp
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ArrayAdapter
@@ -17,18 +16,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    var navController: NavController? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.my_nav) as? NavHostFragment
-
-        navController = navHostFragment?.navController
-
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_nav)
-        navController?.let { NavigationUI.setupWithNavController(bottomNavigationView, it) }
 
         val posts = listOf(
             Post("Laptop", 999.99, "Electronics", "https://example.com/laptop.jpg"),
@@ -42,22 +32,4 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = PostAdapter(posts)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                navController?.popBackStack()
-                true
-            }
-            else -> {
-                navController?.let { NavigationUI.onNavDestinationSelected(item, it) } ?: false
-            }
-        }
-    }
-
-
-//    private fun makeCurrentFragment(fragment: Fragment) =
-//        supportFragmentManager.beginTransaction().apply {
-//            replace(R.id.my_nav, fragment)
-//            commit()
-//        }
 }
